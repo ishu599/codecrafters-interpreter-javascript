9b23155b1f8fa8ba7463b57712962482b9029928
Ishu <raghuvinder.ishu@capgemini.com>
2024-09-27 20:57:31 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -27,59 +27,60 @@ let hasInvalidToken = false;
  if (fileContent.length !== 0) {
   
   let lines = fileContent.split('\n');
-  for(let i=0; i<lines.length;i++) {
-    for(let j=0;j<lines[i].length; j++) {
-      if (invalidTokens.includes(lines[i][j])) {
+  lines.array.forEach((line,index) => {
+    for(const char of line) {
+      if(invalidTokens.includes(char)) {
         hasInvalidToken = true;
-        console.error('[line ${i+1}]: Error: Unexpected character: ${lines[i][j]}')
+        console.error(`[line ${index + 1}] Error: Unexpected character: ${char}`);
       }
-      switch(lines[i][j]) {
-        case '(':
-          console.log("LEFT_PAREN ( null");
+    switch(char) {
+      case '(':
+        console.log("LEFT_PAREN ( null");
+        break;
+      case ')':
+        console.log("RIGHT_PAREN ) null");
+        break;
+        case '{':
+          console.log("LEFT_BRACE { null");
           break;
-        case ')':
-          console.log("RIGHT_PAREN ) null");
+        case '}':
+          console.log("RIGHT_BRACE } null");
           break;
-          case '{':
-            console.log("LEFT_BRACE { null");
+          case '*':
+            console.log("STAR * null");
             break;
-          case '}':
-            console.log("RIGHT_BRACE } null");
+          case '.':
+            console.log("DOT . null");
             break;
-            case '*':
-              console.log("STAR * null");
-              break;
-            case '.':
-              console.log("DOT . null");
-              break;
-            case ',':
-              console.log("COMMA , null");
-              break;
-            case '+':
-              console.log("PLUS + null");
-              break;
-            case '*':
-              console.log("STAR * null");
-              break;
-            case '-':
-              console.log("MINUS - null");
-              break;
-            case ';':
-              console.log("SEMICOLON ; null");
-              break;
-            case '$':
-              console.log("[line 1] Error: Unexpected character: $");
-              break;
-            case '#':
-              console.log("[line 1] Error: Unexpected character: #");
-              break;
-      }
+          case ',':
+            console.log("COMMA , null");
+            break;
+          case '+':
+            console.log("PLUS + null");
+            break;
+          case '*':
+            console.log("STAR * null");
+            break;
+          case '-':
+            console.log("MINUS - null");
+            break;
+          case ';':
+            console.log("SEMICOLON ; null");
+            break;
+          case '$':
+            console.log("[line 1] Error: Unexpected character: $");
+            break;
+          case '#':
+            console.log("[line 1] Error: Unexpected character: #");
+            break;
     }
   }
+  });
   console.log("EOF  null");
 } else {
   console.log("EOF  null");
 }
+
 
 if(hasInvalidToken) {
   process.exit(65)
