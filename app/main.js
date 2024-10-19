14d89da37cb4e601276ecae9343eece15db3b171
Ishu <raghuvinder.ishu@capgemini.com>
2024-10-19 21:46:14 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -128,6 +128,7 @@ if (fileContent.length !== 0) {
         if (index > -1) {
           token += `${string2.toUpperCase()} ${string2.toLowerCase()} null\n`;
         }
+        else {token += `IDENTIFIER ${string2} null\n`;}
       }
       // identify if the line contains any identiier word
       else if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z') || str[j]=="_") {
